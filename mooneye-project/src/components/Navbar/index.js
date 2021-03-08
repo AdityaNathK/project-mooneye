065cdba461e62b3fbe517b64c1d38912473365ee
Aditya Nath <aditya.k.nath@gmail.com>
2021-03-09 04:14:59 +1030

    implements navbar with button display

--- mooneye-project/src/components/Navbar/index.js
+++ mooneye-project/src/components/Navbar/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import {FaBars} from 'react-icons/fa';
-import {Nav,NavbarContainer,NavLogo,MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements';
+import {Nav,
+    NavbarContainer,
+    NavLogo,MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
@@ -19,7 +27,19 @@ const Navbar = () => {
                         <NavItem>
                             <NavLinks to="about">About</NavLinks>
                         </NavItem>
+                        <NavItem>
+                            <NavLinks to="products">Products</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="contact">Contact</NavLinks>
+                        </NavItem>
                     </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/contact">Contact Us</NavBtnLink>            
+                    </NavBtn>
 
                 </NavbarContainer>
             </Nav>
